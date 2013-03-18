90077b1c2ac1e3db62ae487b3a2ee0eb7709b995
mrb <michaelrbernstein@gmail.com>
2013-03-17 21:48:53 -0400

    Shaping up to push to GitHub

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # graaaph
 
-A Clojure library designed to ... well, that part is up to you.
+graaaph is a Clojure library designed to make it easy to consume, manipulate, and analyze JRuby's AST representation of Ruby code from Clojure.
 
 ## Usage
 
-FIXME
+
 
 ## License
 
-Copyright © 2013 FIXME
+Copyright © 2013 Michael R. Bernstein
 
 Distributed under the Eclipse Public License, the same as Clojure.
--- src/graaaph/core.clj
+++ src/graaaph/core.clj
@@ -1,6 +1,7 @@
 (ns graaaph.core
-  (:import (org.jrubyparser Parser
-                            CompatVersion)
+  (:import (org.jrubyparser CompatVersion
+                            Parser
+                            SourcePosition)
            (org.jrubyparser.parser ParserConfiguration)
            (org.jrubyparser.ast Node)
            (java.io.StringReader))
@@ -9,145 +10,38 @@
             [clojure.algo.monads :as m
               :only (domonad maybe-m)]))
 
+;; Zipper Create Functions
+
+(defn can-have-children? [nodes]
+  (instance? Node nodes))
+
+(defn get-children [nodes]
+  (.childNodes nodes))
+
+(defn make-node [_ c]
+  c)
+
+(defn code-zipper [nodes]
+  (z/zipper can-have-children? get-children make-node nodes))
+
+;; Ruby Parsing
+
 (defn parse-ruby [ruby-string]
   (let [config (ParserConfiguration. 0 (CompatVersion/RUBY1_9))
         reader (java.io.StringReader. ruby-string)
         parser (Parser.)]
     (.parse parser "" reader config)))
 
-(defn can-have-children? [nodes]
-  (instance? Node nodes))
-
-(defn code-zipper [nodes]
-  (z/zipper can-have-children? get-children make-node nodes))
-
-(defn make-node [_ c]
-  c)
-
-(defn tree-match [zipper matcher]
-  (loop [loc zipper]
-    (if (z/end? loc)
-      []
-      (if-let [matcher-result (matcher loc)]
-        loc
-        (recur (z/next loc))))))
+;; Zipper Visitor Functions
 
 (defn tree-visitor [zipper visitor]
   (let [coll '()]
-    (loop [loc zipper]
+    (loop [loc zipper c coll]
       (if (z/end? loc)
-        coll
+        (reverse c)
         (if-let [visitor-result (visitor loc)]
-          (do
-            (if-let
-              [good-node (z/node loc)]
-                (do
-                  (println good-node)))
-            (recur (z/next loc)))))))) ;; visitor-result
-
-(tree-visitor zipped-code #(-> % .))
-
-(defn make-matcher [nodetype]
-  (fn [node]
-    (let [znode (z/node node)]
-      (and
-        (not (nil? znode))
-        (= (str (.getNodeType znode))
-           nodetype)))))
-
-(def newline-matcher (make-matcher "NEWLINENODE"))
-
-(def fornode-matcher (make-matcher "FORNODE"))
-
-(tree-match zipped-code newline-matcher)
-
-(tree-match zipped-code fornode-matcher)
-
-(defn get-location [node]
-  (-> node
-      .getPosition))
-
-(defn tree-match-with-position [zipper matcher]
-  (-> (tree-match zipper matcher)
-      z/node
-      .getPosition))
-
-(tree-match-with-position zipped-code fornode-matcher)
-
-(tree-to-vec zipped-code)
-
-(def data (slurp "test/graaaph/testdata.rb"))
-
-(def data "class Dude; @@cool = \"dude\"; def speak; @@cool; end; end ;")
-
-; class Dude
-;   @@cool = "dude"
-;
-;   def speak
-;     @@cool
-;   end
-; end
-;
-; (RootNode,
-;   (NewlineNode,
-;     (ClassNode,
-;       (Colon2ImplicitNode:Dude),
-;       (BlockNode,
-;         (NewlineNode,
-;           (ClassVarAsgnNode:@@cool, (StrNode))),
-;         (NewlineNode,
-;           (DefnNode:speak,
-;             (ArgumentNode:speak),
-;             (ArgsNode),
-;             (NewlineNode,
-;               (ClassVarNode:@@cool))))))))>
-
-(def data "for i in (1..10) do; p i; end")
-
-; Find Class Variables
-; Find for loops
-
-(def c (parse-ruby data))
-
-(def zipped-code (code-zipper c))
-
-(def flattenize
-     (fn [tree]
-       (letfn [(flatten-zipper [so-far zipper]
-                 (println so-far)
-                 (cond (z/end? zipper)
-                       so-far
-                       ;(z/branch? zipper)
-                       ;(flatten-zipper so-far (z/next zipper))
-                       :else
-                       (flatten-zipper (cons (z/node zipper) so-far)
-                                       (z/next zipper))))]
-         (reverse (flatten-zipper '() tree)))))
-
-(def f (flattenize zipped-code))
-
-(filter #(not (= '() %)) (map (fn [n]
-       (cond (nil? n)
-         '()
-         :else
-         (-> n
-             .getNodeType
-             str))) f))
-
-(defn make-matcher [nodetype]
-  (fn [node]
-    (let [znode (z/node node)]
-      (and
-        (not (nil? znode))
-        (= (str (.getNodeType znode))
-           nodetype)))))
-
-(defn matcho [nodetype node q]
-  (let [znode (z/node node)]
-    (and
-      (not (nil? znode))
-      (= (str (.getNodeType znode))
-         nodetype))))
+          (recur (z/next loc) (cons visitor-result c))
+          (recur (z/next loc) c))))))
 
 (defn tree-match [zipper matcher]
   (loop [loc zipper]
@@ -157,15 +51,32 @@
         loc
         (recur (z/next loc))))))
 
-(defn treematchero [zipper matcher q]
-  (loop [loc zipp
+;; Removes nil from visitor fns
+(defn safe-visit [v]
+  (fn [x]
+    (m/domonad m/maybe-m
+       [mx (first x)
+        mv v
+        :when (not (nil? mx))]
+        (mv mx))))
 
-(l/defne orderpreservedo [x y l]
-  ([_ _ [x . r]] (l/membero y r))
-  ([_ _ [_ . r]] (orderpreservedo x y r)))
+(defn invalid-ast-node? [node]
+  (= (class node) org.jrubyparser.SourcePosition$1))
 
-(l/run* [q]
-   (l/fresh [l]
-     (l/== l (l/lvars 5))
-     (orderpreservedo :a :b l)
-     (l/== q l)))
+(defn get-position-data [node]
+  (let [position (.getPosition node)]
+    (cond
+      (invalid-ast-node? position) {}
+      :else
+      (into {}
+        [[:file   (.getFile position)]
+         [:start-line   (.getStartLine position)]
+         [:end-line     (.getEndLine position)]
+         [:start-offset (.getStartOffset position)]
+         [:end-offset   (.getEndOffset position)]]))))
+
+(def zipper-to-map
+  (tree-visitor zipped-code (safe-visit (fn [node]
+                                        (into {}
+                                          [[:position (get-position-data node)]
+                                           [:type (-> node .getNodeType str)]])))))
