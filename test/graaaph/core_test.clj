b8ca6a05c72b7559a5a0b53e4986308384243819
mrb <michaelrbernstein@gmail.com>
2013-03-19 08:00:20 -0400

    Cleanup

--- test/graaaph/core_test.clj
+++ test/graaaph/core_test.clj
@@ -12,10 +12,17 @@
 (deftest simple-parse-test
   (testing "Basic def parse"
     (is (= (parse (:simple data))
-           '({:position {:file "", :start-line 0, :end-line 0, :start-offset 0, :end-offset 14}, :type "ROOTNODE", :value "", :name ""}
-             {:position {:file "", :start-line 0, :end-line 0, :start-offset 0, :end-offset 14}, :type "NEWLINENODE", :value "", :name ""}
-             {:position {:file "", :start-line 0, :end-line 0, :start-offset 0, :end-offset 14}, :type "DEFNNODE", :value "", :name "a"}
-             {:position {:file "", :start-line 0, :end-line 0, :start-offset 4, :end-offset 5}, :type "ARGUMENTNODE", :value "", :name "a"}
-             {:position {:file "", :start-line 0, :end-line 0, :start-offset 6, :end-offset 6}, :type "ARGSNODE", :value "", :name ""}
-             {:position {:file "", :start-line 0, :end-line 0, :start-offset 6, :end-offset 11}, :type "NEWLINENODE", :value "", :name ""}
-             {:position {:file "", :start-line 0, :end-line 0, :start-offset 6, :end-offset 10}, :type "STRNODE", :value "ok", :name ""})))))
+           '({:position {:file "", :start-line 0, :end-line 0, :start-offset 0, :end-offset 14},
+              :type "ROOTNODE", :value "", :name ""}
+             {:position {:file "", :start-line 0, :end-line 0, :start-offset 0, :end-offset 14},
+              :type "NEWLINENODE", :value "", :name ""}
+             {:position {:file "", :start-line 0, :end-line 0, :start-offset 0, :end-offset 14},
+              :type "DEFNNODE", :value "", :name "a"}
+             {:position {:file "", :start-line 0, :end-line 0, :start-offset 4, :end-offset 5},
+              :type "ARGUMENTNODE", :value "", :name "a"}
+             {:position {:file "", :start-line 0, :end-line 0, :start-offset 6, :end-offset 6},
+              :type "ARGSNODE", :value "", :name ""}
+             {:position {:file "", :start-line 0, :end-line 0, :start-offset 6, :end-offset 11},
+              :type "NEWLINENODE", :value "", :name ""}
+             {:position {:file "", :start-line 0, :end-line 0, :start-offset 6, :end-offset 10},
+              :type "STRNODE", :value "ok", :name ""})))))
