f8132df6dd25efa2fd4d9e0f257550efaae034fb
mrb <michaelrbernstein@gmail.com>
2013-05-07 21:06:55 -0400

    Make tests work

---
+++ examples/examples.clj
@@ -0,0 +1,46 @@
+(ns graaaph.core-examples
+  (:use graaaph.core))
+
+(def data {:simple          "def a;'ok';end"
+           :args            "class Dude; def initialize(cool=nil); @cool = cool; end; end; d = Dude.new(:cool);"
+           :class-variable  "class Dude; @@cool = \"dude\"; def speak; @@cool; end; end"
+           :dup-method      "class Dude; def a; \"first\"; end; def a; \"second\"; end; end"
+           :for-loop        "for i in (1..10) do; p i; end"
+           :symbol-to_proc  "[1,2,3].map(&:to_s)"
+           :simple-map      "[1,2,3].map{|x| x.to_s}"
+           :openstruct      "OpenStruct.new"
+           :add             "1+1"})
+
+(def ruby-code "class Dude
+                  def awesome
+                    'first awesome'
+                  end
+                  #
+                  def cool
+                    'not awesome'
+                  end
+                  #
+                  def awesome
+                    'second awesome'
+                  end
+                  #
+                  def bro
+                  end
+                  #
+                  def bro
+                  end
+                end")
+
+(view-ruby-ast ruby-code)
+
+(defn get-duplicate-method-names [ruby-code]
+  (let [nodes   (parse-ruby-code ruby-code)
+        results (l/run* [q]
+                  (l/fresh [a b c d]
+                    (l/== a nodes)
+                    (nodetypeo a "DEFNNODE" c)
+                    (dupeo c d)
+                    (l/== q d)))]
+        results))
+
+(get-duplicate-method-names ruby-code)
--- test/graaaph/core_test.clj
+++ test/graaaph/core_test.clj
@@ -2,47 +2,22 @@
   (:use clojure.test
         graaaph.core))
 
-(def data {:simple          "def a;'ok';end"
-           :file            (slurp "test/graaaph/testdata.rb")
-           :args            "class Dude; def initialize(cool=nil); @cool = cool; end; end; d = Dude.new(:cool);"
-           :class-variable  "class Dude; @@cool = \"dude\"; def speak; @@cool; end; end"
-           :dup-method      "class Dude; def a; \"first\"; end; def a; \"second\"; end; end"
-           :for-loop        "for i in (1..10) do; p i; end"
-           :symbol-to_proc  "[1,2,3].map(&:to_s)"
-           :simple-map      "[1,2,3].map{|x| x.to_s}"
-           :openstruct      "OpenStruct.new"
-           :add             "1+1"})
+(def test-data {:simple          "def a;'ok';end"
+                :args            "class Dude; def initialize(cool=nil); @cool = cool; end; end; d = Dude.new(:cool);"
+                :class-variable  "class Dude; @@cool = \"dude\"; def speak; @@cool; end; end"
+                :dup-method      "class Dude; def a; \"first\"; end; def a; \"second\"; end; end"
+                :for-loop        "for i in (1..10) do; p i; end"
+                :symbol-to_proc  "[1,2,3].map(&:to_s)"
+                :simple-map      "[1,2,3].map{|x| x.to_s}"
+                :openstruct      "OpenStruct.new"
+                :add             "1+1"})
 
-(def ruby-code "class Dude
-                  def awesome
-                    'first awesome'
-                  end
-                  #
-                  def cool
-                    'not awesome'
-                  end
-                  #
-                  def awesome
-                    'second awesome'
-                  end
-                  #
-                  def bro
-                  end
-                  #
-                  def bro
-                  end
-                end")
-
-(view-ruby-ast ruby-code)
-
-(defn get-duplicate-method-names [ruby-code]
-  (let [nodes   (parse-ruby-code ruby-code)
-        results (l/run* [q]
-                  (l/fresh [a b c d]
-                    (l/== a nodes)
-                    (nodetypeo a "DEFNNODE" c)
-                    (dupeo c d)
-                    (l/== q d)))]
-        results))
-
-(get-duplicate-method-names ruby-code)
+(deftest simple-parse
+  (is (= '({:type "ROOTNODE", :value nil, :name nil}
+           {:type "NEWLINENODE", :value nil, :name nil}
+           {:type "DEFNNODE", :value nil, :name "a"}
+           {:type "ARGUMENTNODE", :value nil, :name "a"}
+           {:type "ARGSNODE", :value nil, :name nil}
+           {:type "NEWLINENODE", :value nil, :name nil}
+           {:type "STRNODE", :value nil, :name nil})
+           (parse-ruby-code (:simple test-data)))))
